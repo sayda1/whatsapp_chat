0973695abae21a842683adc847ef08416c9586e8
sayda <sayda>
2017-02-08 10:41:29 -0500

    listo

--- js/main2.js
+++ js/main2.js
@@ -1,20 +1,45 @@
+//buscador
+var search = document.getElementById("search"),
+    food = document.getElementsByClassName("avatar"),
+    forEach = Array.prototype.forEach;
+
+search.addEventListener("keyup", function(e){
+    var choice = this.value;
+  
+    forEach.call(food, function(f){
+        if (f.innerHTML.toLowerCase().search(choice.toLowerCase()) == -1)
+            f.parentNode.style.display = "none";        
+        else
+            f.parentNode.style.display = "block";        
+    });
+}, false);
 //parte logica
-function Chat(_nombre, _imagen)
+function Chat(_nombre, _imagen , _mensaje, _fecha)
 {
 	this.nombre =  _nombre;
 	this.imagenURL = _imagen;
-	this.ultimoMensaje = "";
-	this.horaUltimoMensaje = "";
+	this.ultimoMensaje = _mensaje;
+	this.horaUltimoMensaje = _fecha;
 }
+var fecha= new Date();
 var listaDeChats=
 [
-    new Chat("Maday " , 'http://st-listas.20minutos.es/images/2011-03/279699/2982224_640px.jpg?1334875539'),
-    new Chat("Zeldina " , 'http://www.mejoreslistasyrankings.com/images/0/5/1/actrices_y_cantantes_coreanas_mas_guapas_143150_portada.png'),
-    new Chat("Grace " , 'https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcS3SJ3YqM-VUD7cQeaYZ1I1cXevMy13xirdurJph98bk96lZMoM'),
-    new Chat("Rosa " , 'http://cde.3.elcomercio.pe/ima/0/0/8/3/7/837620/base_image.jpg'),
-    new Chat("Misterio " , 'https://encrypted-tbn1.gstatic.com/images?q=tbn:ANd9GcTygypdZfncEfd46sDd7z_N6buMAscTTfcbe1aIFJ8p9SY7Owbs');
+    new Chat("Maday ", 'http://st-listas.20minutos.es/images/2011-03/279699/2982224_640px.jpg?1334875539' , 'bamos a comer..??', ''),
+    
+    new Chat("Zeldina " , 'http://www.mejoreslistasyrankings.com/images/0/5/1/actrices_y_cantantes_coreanas_mas_guapas_143150_portada.png', 'estoy durmiendo', ''),
+    
+    new Chat("Grace " , 'https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcS3SJ3YqM-VUD7cQeaYZ1I1cXevMy13xirdurJph98bk96lZMoM' , 'si..!!!' , ''),
+    
+    new Chat("Rosa " , 'http://cde.3.elcomercio.pe/ima/0/0/8/3/7/837620/base_image.jpg' , 'donde esta la escoba.?', ''),
+    
+    new Chat("Misterio " , 'https://encrypted-tbn1.gstatic.com/images?q=tbn:ANd9GcTygypdZfncEfd46sDd7z_N6buMAscTTfcbe1aIFJ8p9SY7Owbs' , 'kien soy ..??', ''),
+    
+    new Chat("Cristian " , 'https://ugc.kn3.net/i/origin/http://imagenesfotos.com/wp-content/2008/11/naruto-14.jpg' , '...??' , ''),
+    
+    new Chat("Mona " , 'https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcRhtiTxuRiKDno8MPuW0jGAMsNE2o6TlRgBUyG8dx0kQsP5z_hLkg' , 'porque esa cara.?' , ''),
+    
+    new Chat("Cancervero " , 'http://cdn.noticiaaldia.com.s3.amazonaws.com/wp-content/uploads/2016/03/canse-400x227.jpg' , 'no me salen rimas que no sean reales', '')
 ];
-
 
 //parte visual
 var liItem = null;
@@ -31,7 +56,7 @@ function initChat()
     {
         var htmlChatList='<li><div class="avatar">'+
 				'<img src="' +listaDeChats[i].imagenURL+'" alt="" class="wh-44">' +
-				'<h4 class="w-contact-name">'+listaDeChats[i].nombre+'</h4>' +
+				'<h4 class="w-contact-name" style="color:blue; font-weight:bold;">'+listaDeChats[i].nombre+'</h4>' +
 				'<p class="w-last-message">'+listaDeChats[i].ultimoMensaje+ '</p>' +
 			'</div> ' +
            '<div class="time">'+ listaDeChats[i].horaUltimoMensaje +'</div></li>'; 
@@ -57,7 +82,7 @@ function  onListaChatClick(evt)
     var contactName = evt.currentTarget.getElementsByClassName("w-contact-name")[0].textContent;
     var imgURL= evt.currentTarget.getElementsByClassName("wh-44")[0].src;
     
-   chatCabesa(contactName, imgURL, "Conectado");
+   chatCabesa(contactName, imgURL, "Activo (a)");
 }
 
 function onMensajeKey(event)
@@ -106,7 +131,7 @@ function crearMensajes (_mensaje)
 function crearListaChats (_mensaje)
 {
    var htmlLista=document.getElementById("lista-chat");
-   //var d = new Date();
+   var d = new Date();
     
     if (liItem==null)
     {
@@ -114,10 +139,10 @@ function crearListaChats (_mensaje)
         var htmlChatList =
         '<div class="avatar">' +
 				'<img src="image/logocodeacademy.png" alt="" class="wh-44">' +
-				'<h4 class="w-contact-name">Laboratoria Perú</h4>' +
+				'<h4 class="w-contact-name" style="color:blue; font-weight:bold;">Laboratoria Perú</h4>' +
 				'<p class="w-last-message">' + _mensaje + '</p>' +
 			'</div>' +
-       '<div class="time">14:27</div>';
+       '<div class="time">'+d.getHours()+':'+d.getMinutes()+'</div>';
         
         liItem.innerHTML=htmlChatList;
        htmlLista.insertBefore(liItem, htmlLista.childNodes[0]);
